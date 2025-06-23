93ed02c8ad065fb6bd978ceb00e86fdb0738409a
KIM Museop <museop92@gmail.com>
2025-06-23 23:20:29 +0900

    Delete producer-consumer/target directory

--- producer-consumer/target/debug/deps/producer_consumer-9fdf3228c8a14bf2.d
+++
@@ -1,5 +0,0 @@
-/Users/museop/Workspace/exercies/rust-exercise/producer-consumer/target/debug/deps/libproducer_consumer-9fdf3228c8a14bf2.rmeta: src/main.rs
-
-/Users/museop/Workspace/exercies/rust-exercise/producer-consumer/target/debug/deps/producer_consumer-9fdf3228c8a14bf2.d: src/main.rs
-
-src/main.rs:
--- producer-consumer/target/debug/deps/producer_consumer-fac85820d243396d.d
+++
@@ -1,5 +0,0 @@
-/Users/museop/Workspace/exercies/rust-exercise/producer-consumer/target/debug/deps/libproducer_consumer-fac85820d243396d.rmeta: src/main.rs
-
-/Users/museop/Workspace/exercies/rust-exercise/producer-consumer/target/debug/deps/producer_consumer-fac85820d243396d.d: src/main.rs
-
-src/main.rs:
